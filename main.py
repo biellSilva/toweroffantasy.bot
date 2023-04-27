3d75da4e572fb19a7ddaec3141f9d27d4de74d81
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-04-27 09:24:51 -0300

    feat: added main.py

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 import discord
 import dotenv
+import os
 
 from discord.ext import commands
 
+
 class Dumbot(commands.Bot):
-    def __init__(self) -> None:
-        super().__init__(command_prefix='!', intents=discord.Intents.all())
+
+    def __init__(self):
+        super().__init__(command_prefix='!', intents=discord.Intents.all(), case_insensitive=True, strip_after_prefix=True)
     
-    
+
+    async def on_ready(self):
+        print(f'''
+    {"-"*20}
+        {self.user}
+        {self.status} - {round(self.latency * 1000)}ms
+    {"-"*20}
+        ''')
+
+
+    async def setup_hook(self):
+        self.task = self.loop.create_task(self.wait_until_ready_tasks())
+
+        for folder in os.listdir('./extensions'):
+            if not folder.endswith('.py') and not folder == 'views':
+                for filename in os.listdir(f'./extensions/{folder}'):
+                    if filename.endswith('.py'):
+                        await self.load_extension(f'extensions.{folder}.{filename[:-3]}')
+                        print(f'{folder}.{filename[:-3]} loaded')
+
+
+    async def wait_until_ready_tasks(self):
+        await self.wait_until_ready()
+        await self.change_presence(activity=discord.Activity(
+            type=discord.ActivityType.listening,
+            name=f'!help ou /help'))
+
+
+bot = Dumbot()
+
+bot.run(token=dotenv.get_key(dotenv_path=dotenv.find_dotenv(), key_to_get='token'))
