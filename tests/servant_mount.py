8148cc541e49024209796b73f61029aaa821d268
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-06-16 21:53:49 -0300

    test smart servants and mounts routes

---
+++ tests/servant_mount.py
@@ -0,0 +1,20 @@
+from asyncio import run 
+from pprint import pprint
+
+from src.utils import get_git_data, get_ratelimit
+
+
+async def rate_test():
+    x = await get_ratelimit()
+    pprint(x, indent=2)
+
+
+async def get_mount():
+    x = await get_git_data(name='chaser', data_folder='mounts', data_type='json')
+    pprint(x, indent=2)
+
+async def get_servant():
+    x = await get_git_data(name='angela', data_folder='smart-servants', data_type='json')
+    pprint(x, indent=2)
+
+run(get_servant())
