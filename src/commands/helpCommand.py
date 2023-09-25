c77a28968cbf45fb5994aeda96d60140042cd84e
Gabriel Silva Nobrega <gabrieltkdnobrega63@gmail.com>
2023-09-25 11:06:15 -0300

    fix: embed help command

--- src/commands/helpCommand.py
+++ src/commands/helpCommand.py
@@ -6,6 +6,7 @@ from time import time
 
 from src.views.help_view import NamesView
 from src.utils import data_base, get_ratelimit, get_git_data, get_image
+from src.controller.get_data import get_names
 
 
 class Help_command(commands.Cog):
@@ -34,20 +35,11 @@ class Help_command(commands.Cog):
 
         git_api = await get_ratelimit()
 
-        start_data_timer = time()
-        await get_git_data('Yu lan', 'weapons', data_type='json')
-        get_data_timer = time() - start_data_timer
-
-        start_image_timer = time()
-        await get_image(name=('Yu lan', 'yulan'), data='simulacra')
-        get_image_timer = time() - start_image_timer
 
         em = discord.Embed(color=discord.Colour.dark_embed(),
                             title=f'{self.bot.user.name} Help',
                             description=f'Status: **{self.bot.status}** \n'
                                         f'Latency: **{round(self.bot.latency * 1000)}ms**\n'
-                                        f'Data Latency: **{round(get_data_timer * 1000)}ms**\n'
-                                        f'Image Latency: **{round(get_image_timer * 1000)}ms**\n'
                                         f'Working on **{len(self.bot.guilds)} Guilds**')
         
         em.add_field(name='Git Status', value=(f'> Limit: *{git_api.get("limit")}*\n'
@@ -55,7 +47,9 @@ class Help_command(commands.Cog):
                                                f'> Used: *{git_api.get("used")}*\n'
                                                f'> Reset: <t:{git_api.get("reset")}:R>\n'), inline=False)
 
-        x = ''
+        x = (f'> Simulacras: *{len(await get_names("simulacras"))} itens*\n'
+             f'> Matrices: *{len(await get_names("matrices"))} itens*')
+        
         for data_folder, data_list in data_base.items():
             x += f'> {data_folder.title()}: *{len(data_list)} itens*\n' 
 
--- src/views/help_view.py
+++ src/views/help_view.py
@@ -1,8 +1,7 @@
 import discord
 
-from time import time
-
-from src.utils import get_git_data, get_ratelimit, get_image ,data_base
+from src.utils import get_ratelimit, data_base
+from src.controller.get_data import get_names
 
 
 class NamesView(discord.ui.View):
@@ -15,20 +14,10 @@ class NamesView(discord.ui.View):
 
         git_api = await get_ratelimit()
 
-        start_data_timer = time()
-        await get_git_data('yu lan', 'weapons', data_type='json')
-        get_data_timer = time() - start_data_timer
-
-        start_image_timer = time()
-        await get_image(name=('Yu lan', 'yulan'), data='simulacra')
-        get_image_timer = time() - start_image_timer
-
         em = discord.Embed(color=discord.Colour.dark_embed(),
                             title=f'{bot.user.name} Help',
                             description=f'Status: **{bot.status}** \n'
                                         f'Latency: **{round(bot.latency * 1000)}ms**\n'
-                                        f'Data Latency: **{round(get_data_timer * 1000)}ms**\n'
-                                        f'Image Latency: **{round(get_image_timer * 1000)}ms**\n'
                                         f'Working on **{len(bot.guilds)} Guilds**')
         
         em.add_field(name='Git Status', value=(f'> Limit: *{git_api.get("limit")}*\n'
@@ -36,7 +25,9 @@ class NamesView(discord.ui.View):
                                                f'> Used: *{git_api.get("used")}*\n'
                                                f'> Reset: <t:{git_api.get("reset")}:R>\n'), inline=False)
 
-        x = ''
+        x = (f'> Simulacras: *{len(await get_names("simulacras"))} itens*\n'
+             f'> Matrices: *{len(await get_names("matrices"))} itens*')
+        
         for data_folder, data_list in data_base.items():
             x += f'> {data_folder.title()}: *{len(data_list)} itens*\n' 
 
@@ -65,17 +56,4 @@ class NamesView(discord.ui.View):
 
         await interaction.edit_original_response(embed=em, view=NamesView())
 
-    @discord.ui.button(custom_id='data_help', label='Names', style=discord.ButtonStyle.grey)
-    async def data_names_help_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()
-        em = interaction.message.embeds[0]
-
-        em.description = ''
-        em.clear_fields()
-
-        for data_folder in ('simulacra', 'matrices', 'relics', 'smart-servants', 'mounts'):
-            data_list = await get_git_data(data_folder=data_folder, data_type='names')
-            em.add_field(name=data_folder.title(), value='> '+(', '.join(data_list)), inline=False)
-
-        await interaction.message.edit(embeds=[em], attachments=[], view=NamesView())
     
