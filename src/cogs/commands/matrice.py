96c5b19d4849e0313b19109c449eb3f6e682ebc7
Gabriel <gabrieltkdnobrega63@gmail.com>
2023-12-30 07:29:38 -0300

    feat: matrices command

--- src/cogs/commands/matrice.py
+++ src/cogs/commands/matrice.py
@@ -3,15 +3,21 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
+from src.core.service import API
+from src.infra.entitys import MatriceSimple, Matrix
+
 
 class MatriceCog(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.API = API(simple_model=MatriceSimple,
+                       model=Matrix,
+                       route='matrice')
 
-    @app_commands.command(name='matrice')
+    @app_commands.command(name='matrices')
     @app_commands.rename(id='name')
-    @app_commands.describe(id='Matrice name')
+    @app_commands.describe(id='Matrix name')
     @app_commands.checks.bot_has_permissions(send_messages = True, 
                                              view_channel = True, 
                                              external_emojis = True, 
@@ -23,20 +29,20 @@ class MatriceCog(commands.Cog):
         Matrices (aka Chips) are items that can be attached to weapon slots.
         '''
 
-        # await interaction.response.defer()
+        await interaction.response.defer()
 
-        # matrice = await self.API.get(id=id, locale=interaction.locale)
+        matrice = await self.API.get(id=id, locale=interaction.locale, route='matrices')
 
-        # await interaction.edit_original_response(embed=matrice.embed)
+        await interaction.edit_original_response(embed=matrice.embed)
 
 
-    # @matrices.autocomplete(name='id')
-    # async def matrices_autocomplete(self, interaction: discord.Interaction, current: str) -> list[app_commands.Choice[str]]:
-    #     return [
-    #         app_commands.Choice(name=f'[{matrice.rarity}] {matrice.name}', value=matrice.id) 
-    #         for matrice in await self.API.get_all(locale=interaction.locale) 
-    #         if current.lower() in matrice.name.lower()
-    #     ][:25]
+    @matrices.autocomplete(name='id')
+    async def matrices_autocomplete(self, interaction: discord.Interaction, current: str) -> list[app_commands.Choice[str]]:
+        return [
+            app_commands.Choice(name=f'[{matrice.rarity}] {matrice.name}', value=matrice.id) 
+            for matrice in await self.API.get_all(locale=interaction.locale, route='matrices') 
+            if current.lower() in matrice.name.lower()
+        ][:25]
     
 async def setup(bot: commands.Bot):
     await bot.add_cog(MatriceCog(bot))
