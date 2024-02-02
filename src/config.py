8ae40a68742ea3a952db241d3d69e7c4f9f28009
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-02-02 16:50:23 -0300

    feat: add double elements emojis

--- src/config.py
+++ src/config.py
@@ -1,4 +1,3 @@
-
 from discord.app_commands import Choice
 
 
@@ -9,23 +8,36 @@ FEEDBACK_CATEGORY = 1117793661141393508
 
 ##  EMOJIS
 EMOJIS = {
-    'DPS' : '<:dps:1097910880756310176>',
-    'Tank' : '<:def:1097910876255826050>',
-    'SUP' : '<:sup:1097910879187632308>',
-
-    'CommonAtkAdded': '<:icon_atk:1170591016269643857>',
-    'CritAdded': '<:icon_crit:1170591046049214484>',
-    'ElementDef': '<:icon_resist:1170591062889332907>',
-    'MaxHealthAdded': '<:icon_hp:1170591060330811434>',
-    
-    'Flame': '<:flame:1097910889920864336>',
-    'Ice' : '<:frost:1097910888280895730>',
-    'Thunder' : '<:volt:1097910883155456044>',
-    'Physics' : '<:physic:1097910885529432164>',
-    'Superpower': '<:altered:1097910892504567829>',
-    'PhysicsFlame': '<:phisycal_flame:1145748266055643216>'
+    "DPS": "<:dps:1097910880756310176>",
+    "Tank": "<:def:1097910876255826050>",
+    "SUP": "<:sup:1097910879187632308>",
+    "CommonAtkAdded": "<:icon_atk:1170591016269643857>",
+    "CritAdded": "<:icon_crit:1170591046049214484>",
+    "ElementDef": "<:icon_resist:1170591062889332907>",
+    "MaxHealthAdded": "<:icon_hp:1170591060330811434>",
+    "Flame": "<:flame:1097910889920864336>",
+    "Ice": "<:frost:1097910888280895730>",
+    "Thunder": "<:volt:1097910883155456044>",
+    "Physics": "<:physic:1097910885529432164>",
+    "Superpower": "<:altered:1097910892504567829>",
+    "PhysicsFlame": "<:phisycal_flame:1145748266055643216>",
+    "FlamePhysics": "<:flame_physic:1203064481240977439>",
+    "VoltIce": "<:volt_ice:1203064558797983774>",
+    "IceVolt": "<:ice_volt:1203063805400191017>",
 }
-STAR_EMOJI = '★'
+STAR_EMOJI = "★"
 
-API_LANGS = ['de', 'en', 'es', 'fr', 'id', 'ja', 'pt', 'ru', 'th', 'zh-cn', 'zh-hans-sg']
+API_LANGS = [
+    "de",
+    "en",
+    "es",
+    "fr",
+    "id",
+    "ja",
+    "pt",
+    "ru",
+    "th",
+    "zh-cn",
+    "zh-hans-sg",
+]
 API_LANGS_CHOICE: list[Choice[str]] = [Choice(name=i, value=i) for i in API_LANGS]
