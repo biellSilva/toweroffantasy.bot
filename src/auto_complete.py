b2deb1f238f739fc2dae2c4099ab882b32bd2686
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-11-22 18:43:30 -0300

    feat: implement AutoCompleteHelper for matrix ID autocomplete functionality

---
+++ src/auto_complete.py
@@ -0,0 +1,72 @@
+from typing import Self
+
+from discord import Interaction
+from discord.app_commands import Choice
+from unidecode import unidecode
+
+from src.api.matrices import MatricesService
+from src.types import QualityEnum, RarityEnum
+from src.utils import convert_locale, convert_rarity_to_int, split_matrix_name
+
+
+class AutoCompleteHelper:
+    __instance: Self | None = None
+
+    def __new__(cls) -> Self:
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+    def __init__(self) -> None:
+        self.matrices = MatricesService()
+
+    async def matrix_id_autocomplete(
+        self, interaction: Interaction, current: str
+    ) -> list[Choice[str]]:
+        data = sorted(
+            await self.matrices.get_all_from_cache(
+                lang=convert_locale(interaction.locale)
+            ),
+            key=lambda x: (
+                -convert_rarity_to_int(x.matrices[0].rarity),
+                split_matrix_name(x.matrices[0].name),
+            ),
+        )
+
+        for char in current.split():
+            if unidecode(char).upper() in RarityEnum:
+                data = list(
+                    filter(
+                        lambda x: unidecode(x.matrices[0].rarity).lower()
+                        == unidecode(char).lower(),
+                        data,
+                    )
+                )
+                continue
+
+            if unidecode(char).upper() in QualityEnum:
+                data = list(
+                    filter(
+                        lambda x: unidecode(char).lower()
+                        in unidecode(x.matrices[0].quality).lower()
+                        or unidecode(char).lower() in unidecode(x.quality).lower(),
+                        data,
+                    )
+                )
+                continue
+
+            data = list(
+                filter(
+                    lambda x: unidecode(char).lower()
+                    in unidecode(x.matrices[0].name).lower(),
+                    data,
+                )
+            )
+
+        return [
+            Choice(
+                name=f"[{matrix.matrices[0].rarity}] {split_matrix_name(matrix.matrices[0].name)}",
+                value=matrix.id,
+            )
+            for matrix in data
+        ][:25]
