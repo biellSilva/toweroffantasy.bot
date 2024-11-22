77df442709c9bb1636e999b85d7098c63893b987
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-11-22 18:43:07 -0300

    feat: add enums and utility functions for language, rarity, quality, and emoji handling

---
+++ src/types.py
@@ -0,0 +1,64 @@
+import re
+from enum import Enum, StrEnum
+from typing import Annotated
+
+from discord import PartialEmoji
+from pydantic import AfterValidator
+
+
+class LangsEnum(StrEnum):
+    DE = "de"
+    EN = "en"
+    ES = "es"
+    FR = "fr"
+    ID = "id"
+    JA = "ja"
+    PT = "pt"
+    TH = "th"
+    ZH_CN = "zh-CN"
+
+
+class RarityEnum(StrEnum):
+    N = "N"
+    R = "R"
+    SR = "SR"
+    SSR = "SSR"
+
+
+class QualityEnum(StrEnum):
+    COMMON = "COMMON"
+    RARE = "RARE"
+    EPIC = "EPIC"
+    LEGENDRY = "LEGENDRY"
+    RED = "RED"
+
+
+class EmojisEnum(Enum):
+    CommonAtkAdded = PartialEmoji(name="CommonAtkAdded", id=1309607414231990312)
+    MaxHealthAdded = PartialEmoji(name="MaxHealthAdded", id=1309607643857551450)
+    CritAdded = PartialEmoji(name="CritAdded", id=1309607819221270709)
+    ElementDef = PartialEmoji(name="ElementDef", id=1309608549403463721)
+
+    DPS = PartialEmoji(name="DPS", id=1309612783436042272)
+    SUP = PartialEmoji(name="SUP", id=1309613003989323777)
+    Tank = PartialEmoji(name="Tank", id=1309612877576933436)
+
+    Physics = PartialEmoji(name="Physics", id=1309613593242898572)
+    PhysicsFlame = PartialEmoji(name="PhysicsFlame", id=1309613524435337267)
+
+    Flame = PartialEmoji(name="Flame", id=1309613363717865584)
+    FlamePhysics = PartialEmoji(name="FlamePhysics", id=1309613413181161593)
+
+    Ice = PartialEmoji(name="Ice", id=1309613309544370176)
+    ThunderIce = PartialEmoji(name="ThunderIce", id=1309613162814771293)
+
+    Thunder = PartialEmoji(name="Thunder", id=1309613095945109545)
+    IceThunder = PartialEmoji(name="IceThunder", id=1309613238950035517)
+
+
+def _convert_tag_to_markdown(value: str) -> str:
+    pattern = r"<shuzhi>(.*?)</>"
+    return re.sub(pattern, r"**\1**", value)
+
+
+ParseRegex = Annotated[str, AfterValidator(_convert_tag_to_markdown)]
