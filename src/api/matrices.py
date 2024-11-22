c59971ae62c97258371882f1dd4e6874ef6fd582
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-11-22 18:42:08 -0300

    feat: add MatricesService for managing matrix data with caching and API interaction

---
+++ src/api/matrices.py
@@ -0,0 +1,28 @@
+from discord import Locale
+
+from src.api._base import ApiBaseService
+from src.models.matrices import MatrixSuit
+from src.types import LangsEnum
+from src.utils import convert_locale
+
+
+class MatricesService(ApiBaseService[MatrixSuit]):
+    async def _update_cache(self, lang: LangsEnum) -> None:
+        async with self._get_client() as client:
+            async with client.get("/matrices", params={"lang": lang}) as response:
+                data = [MatrixSuit(**matrix) for matrix in await response.json()]
+                self._cache[lang] = {matrix.id: matrix for matrix in data}
+
+    async def get_matrix(self, lang: Locale, matrix_id: str) -> MatrixSuit:
+        async with self._get_client() as client:
+            async with client.get(
+                f"/matrices/{matrix_id}", params={"lang": convert_locale(lang)}
+            ) as response:
+                return MatrixSuit(**await response.json())
+
+    async def get_matrices(self, lang: Locale) -> list[MatrixSuit]:
+        async with self._get_client() as client:
+            async with client.get(
+                "/matrices", params={"lang": convert_locale(lang)}
+            ) as response:
+                return [MatrixSuit(**matrix) for matrix in await response.json()]
