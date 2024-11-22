d228ad87a25ecb8fed03bbeac854915a2a72c73d
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2024-11-22 18:43:38 -0300

    feat: add BaseView class for custom interaction handling in Discord UI

---
+++ src/views/_base.py
@@ -0,0 +1,27 @@
+from typing import Any
+
+from discord import Interaction, Member, User
+from discord.ui import View
+from discord.ui.item import Item
+
+
+class BaseView(View):
+    def __init__(
+        self, *, timeout: float | None = 180, owner: User | Member | None = None
+    ) -> None:
+        super().__init__(timeout=timeout)
+        self.owner = owner
+
+    def validate_owner(self, interaction: Interaction) -> bool:
+        return interaction.user == self.owner
+
+    async def interaction_check(self, interaction: Interaction) -> bool:
+        return self.validate_owner(interaction)
+
+    async def on_timeout(self) -> None:
+        pass
+
+    async def on_error(
+        self, interaction: Interaction, error: Exception, item: Item[Any]
+    ) -> None:
+        return await super().on_error(interaction, error, item)
