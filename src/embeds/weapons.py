a86685e2da7accb81856a4efa6af2103bfde25ea
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-02-24 17:16:15 -0300

    feat: enhance weapon embeds to include truncated values and add skill embed functionality

--- src/embeds/weapons.py
+++ src/embeds/weapons.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING
 from discord import Embed
 
 from src.types import EmojisEnum
-from src.utils import convert_rarity_to_color, convert_to_emoji
+from src.utils import convert_rarity_to_color, convert_to_emoji, truncate_value
 
 if TYPE_CHECKING:
     from src.models.weapons import Weapon
@@ -19,21 +19,18 @@ class WeaponEmbeds:
         ).set_thumbnail(url=self.weapon.assets.item_large_icon)
 
     def main_embed(self) -> list[Embed]:
-        embed = Embed(
-            title=self.weapon.PREVIEW_NAME,
-            url=self.weapon.URL,
-            color=convert_rarity_to_color(self.weapon.rarity),
-            description=(
-                f"{convert_to_emoji(self.weapon.category.id)} {convert_to_emoji(self.weapon.element.id)} {self.weapon.LIMITED_EMOJI}\n"
-                f"*Shatter* {self.weapon.shatter.to_desc}\n"
-                f"*Charge* {self.weapon.charge.to_desc}\n"
-                f"-# {'\n-# '.join(self.weapon.desc.split('\n'))}\n"
-            ),
+        embed = self._default_embed.copy()
+
+        embed.description = (
+            f"-# **{convert_to_emoji(self.weapon.category.id)} {self.weapon.category.name}**\n"
+            f"-# **{convert_to_emoji(self.weapon.element.id)} {self.weapon.element.name}**\n"
+            f"{self.weapon.LIMITED_EMOJI}\n"
+            f"-# Shatter **{truncate_value(self.weapon.shatter.value)} _{self.weapon.shatter.tier}_**\n"
+            f"-# Charge **{truncate_value(self.weapon.charge.value)} _{self.weapon.charge.tier}_**\n\n"
+            f"{self.weapon.desc}"
         )
 
-        embed.set_thumbnail(url=self.weapon.assets.item_large_icon)
         embed.set_image(url=self.weapon.assets.lottery_drawing)
-
         embed.set_footer(text="Weapon")
 
         return [embed]
@@ -100,10 +97,110 @@ class WeaponEmbeds:
         advance = self.weapon.advancements[index]
 
         embed.description = (
-            f"-# Shatter **{advance.shatter.value} _{advance.shatter.tier}_**\n"
-            f"-# Charge **{advance.charge.value} _{advance.charge.tier}_**\n\n"
+            f"-# Shatter **{truncate_value(advance.shatter.value)} _{advance.shatter.tier}_**\n"
+            f"-# Charge **{truncate_value(advance.charge.value)} _{advance.charge.tier}_**\n\n"
             f"-# **{EmojisEnum.DarkStar.value * (index + 1)}**\n"
             f"{advance.desc}"
         )
 
         return [embed]
+
+    def skill_embed(self, id_: str | None = None) -> list[Embed]:
+        embed = self._default_embed.copy()
+
+        if not self.weapon.skills:
+            embed.description = "-# No skills"
+            embed.set_footer(text="Skill")
+            return [embed]
+
+        if not id_:
+            skill_type = self.weapon.skills[0]
+            skill = skill_type.attacks[0]
+        else:
+            skill_type_, skill_id = id_.split("-")
+
+            skill_type = next(
+                _skill_type
+                for _skill_type in self.weapon.skills
+                if _skill_type.type == skill_type_
+            )
+            skill = next(
+                _skill for _skill in skill_type.attacks if _skill.id == skill_id
+            )
+
+        if skill.values:
+            skill.desc = skill.desc.format(
+                *[truncate_value(value[-1]) for value in skill.values]
+            )
+
+        if len(skill.desc) < 2000:
+            embed.description = (
+                f"**{skill.name}**\n"
+                f"{f'-# _**{" - ".join(skill.tags)}**_\n' if skill.tags else ''}"
+                f"{
+                    f'-# **{f" {EmojisEnum.RightArrow} ".join(skill.operations)}**\n\n'
+                    if skill.operations
+                    else f'-# **{skill_type.name or skill_type.type}**\n\n'
+                }"
+                f"{skill.desc}"
+            )
+
+            embed.set_thumbnail(url=skill.icon).set_footer(text="Skill")
+
+            return [embed]
+
+        embeds: list[Embed] = [embed]
+
+        for ind, desc in enumerate(skill.desc.split("\r\n\r\n")):
+            if ind == 0:
+                embed.description = (
+                    f"**{skill.name}**\n"
+                    f"-# _**{' - '.join(skill.tags)}**_\n"
+                    f"{
+                        f'-# **{f" {EmojisEnum.RightArrow} ".join(skill.operations)}**\n\n'
+                        if skill.operations
+                        else '\n'
+                    }"
+                    f"{desc}"
+                )
+            else:
+                embeds.append(
+                    Embed(
+                        color=convert_rarity_to_color(self.weapon.rarity),
+                        description=desc,
+                    )
+                )
+
+        embeds[-1].set_footer(text="Skill")
+        embeds[0].set_thumbnail(url=skill.icon)
+
+        return embeds
+
+    def fashions_embed(self) -> list[Embed]:
+        if not self.weapon.fashions:
+            embed = self._default_embed.copy()
+            embed.description = "-# No fashions available"
+            embed.set_footer(text="Fashions")
+            return [embed]
+
+        embeds: list[Embed] = []
+
+        for fashion in self.weapon.fashions:
+            embed = Embed(
+                color=convert_rarity_to_color(self.weapon.rarity),
+                description=(
+                    f"**{fashion.name}**\n"
+                    f"-# _**{fashion.display_type_text}**_\n\n"
+                    f"-# {fashion.desc}"
+                ),
+            )
+
+            embed.set_thumbnail(url=fashion.icon)
+
+            embeds.append(embed)
+
+        embeds[0].title = self.weapon.PREVIEW_NAME
+        embeds[0].url = self.weapon.URL
+        embeds[-1].set_footer(text="Fashions")
+
+        return embeds
